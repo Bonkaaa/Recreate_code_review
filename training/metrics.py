95bb98c9f5c705a842b129b04adb23f1e85daa19
Bonkaa <kinnguynhu@gmail.com>
2025-02-22 23:43:45 +0700

    eval trial_updating

--- training/metrics.py
+++ training/metrics.py
@@ -1,4 +1,4 @@
-from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
 
 
 def calculate_metrics(actuals, preds, weights=(0.25, 0.25, 0.25, 0.25)):
@@ -13,7 +13,7 @@ def calculate_metrics(actuals, preds, weights=(0.25, 0.25, 0.25, 0.25)):
 
     smoothing = SmoothingFunction().method1  # Avoid zeros score for missing n-grams
 
-    bleu_score = sentence_bleu(actuals, preds, weights=weights, smoothing_function=smoothing)
+    bleu_score = corpus_bleu(actuals, preds, weights=weights, smoothing_function=smoothing)
 
     return bleu_score
 
@@ -22,12 +22,13 @@ if __name__ == '__main__':
     ref = [["the", "picture", "is", "clicked", "by", "me"],
            ["the", "picture", "was", "clicked", "by", "me"]
     ] # List of lists (each reference should be tokenized)
-    candidate = ["the", "picture", "the", "picture", "by", "me"]  # Tokenized candidate sentence
+    candidate = [["the", "picture", "the", "picture", "by", "me"],
+                 ["the", "picture", "a", "picture", "in", "me"]]  # Tokenized candidate sentence
 
     # Compute BLEU score
     score = calculate_metrics(ref, candidate)
     print(f"BLEU Score: {score:.4f}")
 
     # Verify correctness
-    expected_score = sentence_bleu(ref, candidate, weights=(0.25, 0.25, 0.25, 0.25), smoothing_function=SmoothingFunction().method1)
+    expected_score = corpus_bleu(ref, candidate, weights=(0.25, 0.25, 0.25, 0.25), smoothing_function=SmoothingFunction().method1)
     print(f"Expected BLEU Score: {expected_score:.4f}")
