1c4e8de00301cd11737e5ad3b8852665dd2c97b5
Bonkaaa <kinnguynhu@gmail.com>
2025-02-27 15:07:45 +0700

    apply LoRA

--- training/lora_config.py
+++ training/lora_config.py
@@ -5,7 +5,7 @@ def get_lora_config():
     return LoraConfig(
         r=8,  # Low-rank dimension
         lora_alpha=16,  # Scaling factor
-        target_modules=["q_proj", "v_proj"],  # Layers to apply LoRA
+        target_modules=["q", "k", "v", "o", "wi_0", "wi_1"],  # Layers to apply LoRA
         lora_dropout=0.1,
         bias="none",
         task_type="SEQ_2_SEQ_LM"
