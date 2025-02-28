87108fc408bceac36cd504e4876712cea76aedfb
Bonkaa <kinnguynhu@gmail.com>
2025-02-28 14:58:28 +0700

    update test.py and test.sh to change model directory and data paths for compatibility with Kaggle environment

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PROJECT="repo-specific"
-DataDir="/raid/data/dmtran/GovTech/repo-specific/top_5_most"
+PROJECT="Automated code review"
+DataDir="/kaggle/input/dataaa/t5_data"
 
 #TYPE='roberta'
 #MODEL='microsoft/codebert-base'
@@ -22,13 +22,13 @@ DataDir="/raid/data/dmtran/GovTech/repo-specific/top_5_most"
 # TOKENIZER='microsoft/unixcoder-base'
 # OUTPUT_DIR=./outputs/unixcoder/
 
-CUDA_VISIBLE_DEVICES=1 python -m test \
+CUDA_VISIBLE_DEVICES=0,1 python -m test \
     --project ${PROJECT} \
-    --model_dir = "C:\neutral_network\outputs\codet5\checkpoint-epoch-10\Code_review_generation\Salesforce\codet5-base" \
+    --model_dir = "/kaggle/input/codet5/transformers/default/1/outputs/codet5/checkpoint-epoch-10/Code_review_generation/Salesforce/codet5-base" \
     --output_dir=${OUTPUT_DIR} \
     --model_type=${TYPE} \
     --tokenizer_name=${TOKENIZER} \
     --model_name_or_path=${MODEL} \
-    --test_data_file= "C:\neutral_network\DATA\Review-Comment-Generation\t5_data\test.jsonl" \
+    --test_data_file= "/kaggle/input/test-data/test.jsonl" \
     --block_size 256 \
     --num_proc 4 \
