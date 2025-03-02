8152bb94376249745c9029c8d4478cd51dbca7c1
manhtdd <manh.td120901@gmail.com>
2025-03-02 13:28:45 +0800

    update training script to change data directory and increase batch sizes

--- scripts/training.sh
+++ scripts/training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT="Code_review_generation"
-DataDir="/kaggle/input/100-datapoint/data"
+DataDir="/raid/data/dmtran/t5_data"
 
 # TYPE='roberta'
 # MODEL='microsoft/codebert-base'
@@ -34,8 +34,8 @@ CUDA_VISIBLE_DEVICES=0,1 accelerate launch train.py \
     --epoch 10 \
     --do_eval \
     --block_size 256 \
-    --train_batch_size 4 \
-    --eval_batch_size 4 \
+    --train_batch_size 128 \
+    --eval_batch_size 128 \
     --learning_rate 2e-5 \
     --warmup_steps 1000 \
     --max_grad_norm 1.0 \
