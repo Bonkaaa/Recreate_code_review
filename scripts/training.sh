df3908c5284369e1d0c9bfd6f5b1790ec203a8e3
manhtdd <manh.td120901@gmail.com>
2025-03-02 13:29:46 +0800

    update training script and configuration for improved data handling and training parameters

--- scripts/training.sh
+++ scripts/training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT="Code_review_generation"
-DataDir="/kaggle/input/100-datapoint/data"
+DataDir="/raid/data/dmtran/t5_data"
 
 # TYPE='roberta'
 # MODEL='microsoft/codebert-base'
@@ -31,11 +31,11 @@ CUDA_VISIBLE_DEVICES=0,1 accelerate launch train.py \
     --model_name_or_path=${MODEL} \
     --train_data_file=${DataDir}"/train.jsonl" \
     --eval_data_file=${DataDir}"/val.jsonl" \
-    --epoch 10 \
+    --epoch 5 \
     --do_eval \
     --block_size 256 \
-    --train_batch_size 4 \
-    --eval_batch_size 4 \
+    --train_batch_size 128 \
+    --eval_batch_size 128 \
     --learning_rate 2e-5 \
     --warmup_steps 1000 \
     --max_grad_norm 1.0 \
