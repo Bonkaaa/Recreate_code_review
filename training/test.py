66ba2964d4a181713e61b3035e7039e8fe1cb6f8
Bonkaa <kinnguynhu@gmail.com>
2025-03-03 16:39:47 +0700

    update test.py

--- training/test.py
+++ training/test.py
@@ -30,7 +30,7 @@ def test_model(args, model_dir, test_dataloader, model, tokenizer, accelerator):
 
     # Load the model and tokenizer
     model = accelerator.prepare(model)
-    load_model(model, os.path.join(model_dir, "model.safetensors"), device=args.device)
+    load_model(model, os.path.join(model_dir, "model.safetensors"))
 
     # Initialize the lists to store the generated and actual comments
     all_generated_comments = []
