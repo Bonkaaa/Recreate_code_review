0598fd65fe0bada59a103e10e6bbad343f0cfc29
Bonkaa <kinnguynhu@gmail.com>
2025-03-12 14:26:13 +0700

    update test.py

--- training/test.py
+++ training/test.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
                                                 cache_dir=args.cache_dir if args.cache_dir else None)
 
     # Load the original model
-    original_model = T5ForConditionalGeneration.from_pretrained(args.model_name_or_path)
+    original_model = T5ForConditionalGeneration.from_pretrained(args.model_dir)
 
     # Load the test dataset
     test_data = load_jsonl(args.test_data_file)[:10] # Load only 10 samples for testing
