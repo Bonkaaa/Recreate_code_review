ac898c85fe7af99eaa10ff928fc05b710345829a
Bonkaa <kinnguynhu@gmail.com>
2025-03-12 15:40:22 +0700

    update file and add test.py

--- training/checkpoint.py
+++ training/checkpoint.py
@@ -2,26 +2,45 @@ import os
 from utils import logging
 from args_parse import main as args_parse
 
-def load_checkpoint(args, accelerator, prefix):
+
+def load_checkpoint(args, model, original_model, accelerator, prefix):
     try:
+        # Load model
         output_dir = f'{args.output_dir}/{prefix}/{args.project}/{args.model_dir}'
-        if os.path.exists(output_dir):
+        if not os.path.exists(output_dir):
             if accelerator.is_main_process:
-                logging.debug(f"Found {output_dir}. Load back to accelerator.")
-            accelerator.load_state(output_dir)
-        else:
-            if accelerator.is_main_process:
-                logging.debug("No accelerator to load")
+                raise FileNotFoundError(f"Model checkpoint not found: {output_dir}")
+
+        accelerator.wait_for_everyone()
+        unwrapped_model = accelerator.unwrap_model(model)
+        unwrapped_model.from_pretrained(
+            model=original_model,
+            model_id=output_dir,
+            is_main_process=accelerator.is_main_process
+        )
+
+        if accelerator.is_main_process:
+            logging.info(f"Found {output_dir}. Load back to accelerator.")
+
     except Exception as e:
         if accelerator.is_main_process:
-            logging.error(f"Save checkpoint failed: {e}")
+            logging.error(f"Load checkpoint failed: {e}")
 
-def save_checkpoint(args, accelerator, prefix):
+
+def save_checkpoint(args, model, accelerator, prefix):
     try:
         output_dir = f'{args.output_dir}/{prefix}/{args.project}/{args.model_dir}'
         os.makedirs(output_dir, exist_ok=True)
+
+        # Save model
         accelerator.wait_for_everyone()
-        accelerator.save_state(output_dir)
+        unwrapped_model = accelerator.unwrap_model(model)
+        unwrapped_model.save_pretrained(
+            output_dir,
+            is_main_process=accelerator.is_main_process,
+            save_function=accelerator.save,
+        )
+
         if accelerator.is_main_process:
             logging.info(f"Saving model to {output_dir}")
     except Exception as e:
--- training/evaluating.py
+++ training/evaluating.py
@@ -7,7 +7,7 @@ from tqdm.auto import tqdm
 from args_parse import *
 from transformers import RobertaTokenizer
 
-def evaluate(args, model, eval_dataloader, tokenizer:RobertaTokenizer, criterion, accelerator = None):
+def evaluate(args, model, eval_dataloader, tokenizer: RobertaTokenizer, criterion, accelerator=None):
     eval_output_dir = args.output_dir
     if not os.path.exists(eval_output_dir):
         os.makedirs(eval_output_dir)
@@ -29,28 +29,29 @@ def evaluate(args, model, eval_dataloader, tokenizer:RobertaTokenizer, criterion
         target_ids = batch['target_ids'].to(args.device)
         with torch.no_grad():
 
-            #forward
-            outputs = model(input_ids = in_ids, attention_mask = in_masks, labels = target_ids)
+            # forward
+            outputs = model(input_ids=in_ids, attention_mask=in_masks, labels=target_ids)
 
-            #Compute loss
+            # Compute loss
             loss = criterion(outputs.logits.view(-1, outputs.logits.size(-1)), target_ids.view(-1))
-            
+
             if accelerator:
                 loss = accelerator.gather(loss)
                 outputs = accelerator.gather(outputs)
                 target_ids = accelerator.gather(target_ids)
-                
-            #decode the generated comments
-            generated_comments = tokenizer.batch_decode(outputs.logits.argmax(dim=-1), skip_special_tokens=True)  # decode the generated comments   
+
+            # decode the generated comments
+            generated_comments = tokenizer.batch_decode(outputs.logits.argmax(dim=-1),
+                                                        skip_special_tokens=True)  # decode the generated comments
             actual_comments = tokenizer.batch_decode(target_ids, skip_special_tokens=True)  # decode the actual comments
-            
+
             eval_loss += loss.mean().item()
 
             # calculate BLEU score
             if accelerator.is_main_process:
                 logging.debug(f"Generated comments: {generated_comments}")
-                logging.debug(f"Actual comments: {actual_comments}") 
-                
+                logging.debug(f"Actual comments: {actual_comments}")
+
             bleu_score = calculate_bleu_score(actual_comments, generated_comments)
             EM_score = calculate_exact_match_score(actual_comments, generated_comments)
             if accelerator.is_main_process:
@@ -63,7 +64,7 @@ def evaluate(args, model, eval_dataloader, tokenizer:RobertaTokenizer, criterion
 
         nb_eval_steps += 1
 
-    #calculate avg
+    # calculate avg
     avg_bleu_score = np.mean(all_bleu_score) if all_bleu_score else 0
     avg_EM_score = np.mean(all_em_score) if all_em_score else 0
     eval_loss = eval_loss / nb_eval_steps
